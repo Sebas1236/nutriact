8ff6ffe56923add78d526687b804687246523e96
Sebas1236 <cptsebas@gmail.com>
2023-04-16 20:46:47 -0500

    Added light/dark/system mode icons

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AiOutlineMenu } from 'react-icons/ai'
+import { FiAirplay, FiMoon, FiSun } from 'react-icons/fi'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
@@ -21,7 +22,13 @@ const UserMenu: React.FC<UserMenuProps> = ({
     const loginModal = useLoginModal();
 
     const [isOpen, setIsOpen] = useState(false);
-    
+
+    const [modeIsOpen, setModeIsOpen] = useState(false);
+
+    const toggleMode = useCallback(() =>
+        setModeIsOpen(!modeIsOpen),
+        [modeIsOpen]);
+
     const toggleOpen = useCallback(() =>
         setIsOpen(!isOpen),
         [isOpen]);
@@ -30,7 +37,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
         <div className='relative'>
             <div className='flex flex-row items-center gap-3'>
                 <div
-                    onClick={() => { }}
+                    onClick={toggleMode}
                     className='
                         hidden
                         md:block
@@ -42,10 +49,32 @@ const UserMenu: React.FC<UserMenuProps> = ({
                         hover:bg-neutral-100
                         transition 
                         cursor-pointer
+                        text-sky-500
                     '
                 >
-                    NutriAct -
+                    <FiSun />
                 </div>
+                {
+                    modeIsOpen && (
+                        <>
+                            <ul tabIndex={0} className='absolute z-50 top-full right-0 bg-white rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-slate-700 font-semibold dark:bg-slate-800 dark:ring-0 dark:highlight-white/5 dark:text-slate-300 mt-4'>
+                                <li className='py-1 px-2 flex items-center cursor-pointer text-sky-500'>
+                                    <FiSun />
+                                    <span className='px-4'>Light</span>
+                                </li>
+                                <li className='py-1 px-2 flex items-center cursor-pointer text-slate-800'>
+                                    <FiMoon />
+                                    <span className='px-4'>Dark</span>
+                                </li>
+                                <li className='py-1 px-2 flex items-center cursor-pointer text-slate-800'>
+                                    <FiAirplay />
+                                    <span className='px-4'>System</span>
+                                </li>
+                            </ul>
+                        </>
+                    )
+                }
+
                 <div
                     onClick={toggleOpen}
                     className='
