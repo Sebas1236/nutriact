440a96bb757268e6d0c8ee74b40f65c313701e35
Sebas1236 <cptsebas@gmail.com>
2023-06-03 17:01:00 -0600

    Profile settings component created for the edit of public information of the user

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -5,66 +5,75 @@ import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { SafeUser } from "@/app/types";
 import Avatar from "./Avatar";
+import SettingsModal from "./SettingsModal";
+import { User } from "@prisma/client";
 
 interface DesktopSidebarProps {
-  currentUser: SafeUser | null;
+  currentUser: User;
 }
 
 const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
-      <nav
-        className="
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+        <nav
+          className="
           mt-4
           flex
           flex-col
           justify-between
         "
-      >
-        <ul
-          role="list"
-          className="
+        >
+          <ul
+            role="list"
+            className="
             flex
             flex-col 
             items-center
             space-y-1
           "
-        >
-          {routes.map((route) => (
-            <DesktopItem
-              key={route.label}
-              href={route.href}
-              label={route.label}
-              icon={route.icon}
-              active={route.active}
-              onClick={route.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
-      <nav
-        className="
+          >
+            {routes.map((route) => (
+              <DesktopItem
+                key={route.label}
+                href={route.href}
+                label={route.label}
+                icon={route.icon}
+                active={route.active}
+                onClick={route.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <nav
+          className="
             mt-4
             flex
             flex-col
             items-center
             justify-between
       "
-      >
-        <div
-          onClick={() => setIsOpen(!isOpen)}
-          className="
+        >
+          <div
+            onClick={() => setIsOpen(!isOpen)}
+            className="
             cursor-pointer
             hover:opacity-75
             transition
           "
-        >
-          <Avatar user={currentUser} />
-        </div>
-      </nav>
-    </div>
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
