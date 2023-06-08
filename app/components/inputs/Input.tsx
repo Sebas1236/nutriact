73038b6f96dd11284f9fd0aa428314b49f6d997a
Sebas1236 <cptsebas@gmail.com>
2023-06-08 11:13:38 -0600

    Added simple form validation, messages for required and for email input type

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,50 +1,45 @@
-'use client';
+"use client";
 
-import { 
-    FieldErrors, 
-    FieldValues, 
-    UseFormRegister 
-} from "react-hook-form";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
-    id: string;
-    label: string;
-    type?: string;
-    disabled?: boolean;
-    formatPrice?: boolean;
-    required?: boolean;
-    register: UseFormRegister<FieldValues>,
-    errors: FieldErrors
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
 }
 
 const Input: React.FC<InputProps> = ({
-    id,
-    label,
-    type = 'text',
-    disabled = false,
-    formatPrice = false,
-    register,
-    required = false,
-    errors,
+  id,
+  label,
+  type = "text",
+  disabled = false,
+  formatPrice = false,
+  register,
+  required = false,
+  errors,
 }) => {
-    return (
-        <div
-            className='w-full relative'
-        >
-            {formatPrice && (
-                <BiDollar 
-                    size={24}
-                    className="text-neutral-700 absolute top-5 left-2"
-                />
-            )}
-            <input 
-                id={id}
-                disabled={disabled}
-                {...register(id, { required })}
-                placeholder=" "
-                type={type}
-                className={`
+  return (
+    <div className="w-full relative">
+      {formatPrice && (
+        <BiDollar
+          size={24}
+          className="text-neutral-700 absolute top-5 left-2"
+        />
+      )}
+      <input
+        id={id}
+        disabled={disabled}
+        {...(type === "email" && register(id, { pattern: /^\S+@\S+$/i }))}
+        {...register(id, { required: "Este campo es obligatorio" })}
+        placeholder=" "
+        type={type}
+        className={`
                     peer
                     w-full
                     p-4
@@ -57,14 +52,20 @@ const Input: React.FC<InputProps> = ({
                     transition
                     disabled:opacity-70
                     disabled:cursor-not-allowed
-                    ${formatPrice ? 'pl-9' : 'pl-4'}
-                    ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-                    ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+                    mt-2
+                    ${formatPrice ? "pl-9" : "pl-4"}
+                    ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+                    ${
+                      errors[id]
+                        ? "focus:border-rose-500"
+                        : "focus:border-black"
+                    }
                 `}
-            />
-            <label
-                className={`
+      />
+      <label
+        className={`
                     absolute
+                    mt-2
                     text-md
                     duration-150
                     transform
@@ -72,18 +73,27 @@ const Input: React.FC<InputProps> = ({
                     top-5
                     z-10
                     origin-[0]
-                    ${formatPrice ? 'left-9' : 'left-4'}
+                    ${formatPrice ? "left-9" : "left-4"}
                     peer-placeholder-shown:scale-100
                     peer-placeholder-shown:translate-y-0
                     peer-focus:scale-75
                     peer-focus:-translate-y-4
-                    ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+                    ${errors[id] ? "text-rose-500" : "text-zinc-400"}
                 `}
-            >
-                {label}
-            </label>
-        </div>
-    );
+      >
+        {label}
+      </label>
+
+      {errors[id] && (
+        <span className="absolute text-rose-500 text-sm top-16 left-0 mt-3">
+          {/* checamos si es required o email */}
+          {errors[id]!.type === "required"
+            ? "Este campo es obligatorio"
+            : errors[id]?.type === "pattern" && "El correo no es válido"}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Input;
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,145 +1,137 @@
-'use client';
+"use client";
 
-import axios from 'axios';
-import { AiFillGithub } from 'react-icons/ai';
-import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
-import {
-    FieldValues,
-    SubmitHandler,
-    useForm,
-} from 'react-hook-form';
-import useRegisterModal from '@/app/hooks/useRegisterModal';
-import Modal from './Modal';
-import Heading from '../Heading';
-import Input from '../inputs/Input';
-import { toast } from 'react-hot-toast';
-import Button from '../Button';
-import { signIn } from 'next-auth/react';
-import useLoginModal from '@/app/hooks/useLoginModal';
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+import { toast } from "react-hot-toast";
+import Button from "../Button";
+import { signIn } from "next-auth/react";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const registerModal = useRegisterModal();
-    const loginModal = useLoginModal();
-    const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FieldValues>({
-        defaultValues: {
-            name: '',
-            email: '',
-            password: '',
-        }
-    });
-
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true);
-        axios.post('/api/auth/register', data)
-            .then((res) => {
-                registerModal.onClose();
-                console.log(res);
-            })
-            .catch((err) => {
-                toast.error("Algo salió mal.");
-            }).finally(() => {
-                setIsLoading(false);
-            });
-    }
-
-    const toggle = useCallback(() => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/auth/register", data)
+      .then((res) => {
         registerModal.onClose();
-        loginModal.onOpen();
-    }, [registerModal, loginModal]);
+        console.log(res);
+      })
+      .catch((err) => {
+        toast.error("Algo salió mal.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
-    const bodyContent = (
-        <div className='flex flex-col gap-4'>
-            <Heading
-                title='Bienvenido a NutriAct'
-                subtitle='Crea una cuenta!'
-            />
-            <Input
-                id='email'
-                label='Email'
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
-            <Input
-                id='name'
-                label='Nombre'
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
-            <Input
-                id='password'
-                type='password'
-                label='Contraseña'
-                disabled={isLoading}
-                register={register}
-                errors={errors}
-                required
-            />
-        </div>
-    )
+  const toggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal]);
 
-    const footerContent = (
-        <div className='flex flex-col gap-4 mt-3'>
-            <hr />
-            <Button
-                outline
-                label='Continuar con Google'
-                icon={FcGoogle}
-                onClick={() => signIn('google')}
-            />
-            <Button
-                outline
-                label='Continuar con Github'
-                icon={AiFillGithub}
-                onClick={() => signIn('github')}
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Bienvenido a NutriAct" subtitle="Crea una cuenta!" />
+      <Input
+        id="email"
+        label="Email"
+        type="email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Nombre"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Contraseña"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
 
-            />
-            <div
-                className='
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continuar con Google"
+        icon={FcGoogle}
+        onClick={() => signIn("google")}
+      />
+      <Button
+        outline
+        label="Continuar con Github"
+        icon={AiFillGithub}
+        onClick={() => signIn("github")}
+      />
+      <div
+        className="
                     text-neutral-500
                     text-center
                     mt-4
                     font-light
-                '
-            >
-                <div className='justify-center flex flex-row items-center gap-2'>
-                    <div>
-                        ¿Ya tienes una cuenta?
-                    </div>
-                    <div
-                        onClick={toggle}
-                        className='text-neutral-800 cursor-pointer hover:underline'>
-                        Iniciar Sesión
-                    </div>
-                </div>
-            </div>
+                "
+      >
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>¿Ya tienes una cuenta?</div>
+          <div
+            onClick={toggle}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Iniciar Sesión
+          </div>
         </div>
+      </div>
+    </div>
+  );
 
-    )
-
-    return (
-        <Modal
-            disabled={isLoading}
-            isOpen={registerModal.isOpen}
-            title='Regístrate'
-            actionLabel='Continuar'
-            onClose={registerModal.onClose}
-            onSubmit={handleSubmit(onSubmit)}
-            body={bodyContent}
-            footer={footerContent}
-        />
-    );
-}
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Regístrate"
+      actionLabel="Continuar"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
 
 export default RegisterModal;
