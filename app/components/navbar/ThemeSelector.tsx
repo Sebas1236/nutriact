8de7414a374cad7ac4f8f06572f7e5d49d3114f4
Sebas1236 <cptsebas@gmail.com>
2023-06-14 09:15:20 -0600

    Updated ThemeSelector

--- app/components/navbar/ThemeSelector.tsx
+++ app/components/navbar/ThemeSelector.tsx
@@ -4,19 +4,19 @@ const ThemeSelector = () => {
   return (
     <ul
       tabIndex={0}
-      className="absolute z-50 top-full right-0 bg-white rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-slate-700 font-semibold dark:bg-slate-800 dark:ring-0 dark:highlight-white/5 dark:text-slate-300 mt-4"
+      className="absolute z-50 top-full right-0 bg-white rounded-lg ring-1 ring-slate-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm font-semibold dark:ring-0 dark:highlight-white/5 mt-4"
     >
-      <li className="py-1 px-2 flex items-center cursor-pointer text-green-500">
+      <li className="py-1 px-2 flex items-center cursor-pointer hover:bg-neutral-200">
         <FiSun />
         <span className="px-4">Light</span>
       </li>
-      <li className="py-1 px-2 flex items-center cursor-pointer text-green-500">
+      <li className="py-1 px-2 flex items-center cursor-pointer hover:bg-neutral-200">
         <FiMoon />
         <span className="px-4">Dark</span>
       </li>
-      <li className="py-1 px-2 flex items-center cursor-pointer text-green-500">
+      <li className="py-1 px-2 flex items-center cursor-pointer hover:bg-neutral-200">
         <FiAirplay />
-        <span className="px-4">System</span>
+        <span className="px-4">Sistema</span>
       </li>
     </ul>
   );
